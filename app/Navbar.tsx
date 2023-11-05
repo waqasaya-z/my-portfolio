be1f78c16017f085514f736df9d9c20d46626a9f
waqasaya-z <waqasayaz@icloud.com>
2023-11-05 19:29:41 +0500

    Add Start

---
+++ app/Navbar.tsx
@@ -0,0 +1,16 @@
+
+const Navbar = () => {
+  return (
+    <nav className="flex p-4 text-xl font-medium justify-between text-white">
+      <h1> Waqas Ayaz. </h1>
+      <ul className="flex gap-4 mr-5">
+        <li>Start</li>
+        <li>Lab</li>
+        <li>Experience</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
